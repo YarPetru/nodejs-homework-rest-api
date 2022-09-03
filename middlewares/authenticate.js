f81625605c0b4876e6d1d8e2c064e0a78743588d
Yaroslav Petrukhin <75678049+YarPetru@users.noreply.github.com>
2022-09-03 23:36:05 +0300

    error fixed, work token

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,17 +7,16 @@ const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, _, next) => {
   const { authorization = "" } = req.headers;
-  const [bearer, token] = authorization.split("");
+  const [bearer, token] = authorization.split(" ");
 
   if (bearer !== "Bearer") {
     next(RequestError(401, "Not authorized"));
   }
-
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token) {
-      next(RequestError(401, "Not authorized"));
+    if (!user) {
+      next(RequestError(401, "Unauthorized"));
     }
     req.user = user;
     next();
